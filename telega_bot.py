94439ef0fd171535c212c6fa18842f1c4e6e0268
Eugene <mihaylovskiyeugeny@gmail.com>
2020-10-09 18:21:52 +0300

    add and config emoje

--- telega_bot.py
+++ telega_bot.py
@@ -27,25 +27,54 @@ def send_echo(message):
         temperature = w.temperature('celsius')['temp']
         wind = w.wind()['speed']
         current_time = timestamps.now('iso')
+        status = w.status
 
 
-        thunderstorm = u'\U0001F4A8'    # Code: 200's, 900, 901, 902, 905
-        drizzle = u'\U0001F4A7'         # Code: 300's
-        rain = u'\U00002614'            # Code: 500's
-        snowflake = u'\U00002744'       # Code: 600's snowflake
-        snowman = u'\U000026C4'         # Code: 600's snowman, 903, 906
-        atmosphere = u'\U0001F301'      # Code: 700's foogy
-        clearSky = u'\U00002600'        # Code: 800 clear sky
-        fewClouds = u'\U000026C5'       # Code: 801 sun behind clouds
-        clouds = u'\U00002601'          # Code: 802-803-804 clouds general
-        hot = u'\U0001F525'             # Code: 904
-        defaultEmoji = u'\U0001F300'    # default emojis
+        thunderstorm = u'\U0001F4A8'    
+        drizzle = u'\U0001F4A7'         
+        rain = u'\U00002614'            
+        snowflake = u'\U00002744'       
+        snowman = u'\U000026C4'         
+        atmosphere = u'\U0001F301'     
+        clearSky = u'\U00002600'       
+        fewClouds = u'\U000026C5'       
+        clouds = u'\U00002601'          
+        hot = u'\U0001F525'             
+        defaultEmoji = u'\U0001F60A'    
+
+
+        def set_emojo (weather_status):
+            try:
+                if str(weather_status) == "Thunderstorm":
+                    return thunderstorm
+                elif str(weather_status) == "Drizzle":
+                    return drizzle
+                elif str(weather_status) == "Rain":
+                    return rain
+                elif str(weather_status) == "Snowflake":
+                    return snowflake
+                elif str(weather_status) == "Snowman":
+                    return snowman
+                elif str(weather_status) == "Atmosphere":
+                    return atmosphere
+                elif str(weather_status) == "ClearSky" or "Clear":
+                    return clearSky
+                elif str(weather_status) == "FewClouds":
+                    return fewClouds
+                elif str(weather_status) == "Clouds":
+                    return clouds
+                elif str(weather_status) == "Hot":
+                    return hot
+                else:
+                    return defaultEmoji        
+            except Exception as e:
+                print(e)
 
 
         answer = "Сейчас: " + str(current_time) + "\n\n"
         answer += "Температура в " + message.text + ": " + str(temperature) + " ℃" + "\n"
         answer += "Ветер: " + str(wind) + " м/с " + "\n"
-        answer += "В общем - " + w.detailed_status + emojo + "\n\n"
+        answer += "В общем - " + w.detailed_status + set_emojo(status) + "\n\n"
 
         if temperature < 10:
             answer += "Сейчас очень холодно, одевайся!"
@@ -55,6 +84,7 @@ def send_echo(message):
             answer += "Сейчас тепло, одевай что хочешь."
 
         bot.send_message(message.chat.id, answer)
+        print(status)
     except Exception as e:
         error = str(e)
         bot.send_message(message.chat.id, error + ". Please try again!")
@@ -62,3 +92,5 @@ def send_echo(message):
 
 
 bot.polling(none_stop=True)
+
+
