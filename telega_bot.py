a5d78e420be2e99de3f53d4d0478cecad86d354c
Eugene <mihaylovskiyeugeny@gmail.com>
2020-10-09 17:44:09 +0300

    add changes + emojes

--- telega_bot.py
+++ telega_bot.py
@@ -27,12 +27,25 @@ def send_echo(message):
         temperature = w.temperature('celsius')['temp']
         wind = w.wind()['speed']
         current_time = timestamps.now('iso')
-        # next_three_hours = timestamps.next_three_hours()
+
+
+        thunderstorm = u'\U0001F4A8'    # Code: 200's, 900, 901, 902, 905
+        drizzle = u'\U0001F4A7'         # Code: 300's
+        rain = u'\U00002614'            # Code: 500's
+        snowflake = u'\U00002744'       # Code: 600's snowflake
+        snowman = u'\U000026C4'         # Code: 600's snowman, 903, 906
+        atmosphere = u'\U0001F301'      # Code: 700's foogy
+        clearSky = u'\U00002600'        # Code: 800 clear sky
+        fewClouds = u'\U000026C5'       # Code: 801 sun behind clouds
+        clouds = u'\U00002601'          # Code: 802-803-804 clouds general
+        hot = u'\U0001F525'             # Code: 904
+        defaultEmoji = u'\U0001F300'    # default emojis
+
 
         answer = "Сейчас: " + str(current_time) + "\n\n"
         answer += "Температура в " + message.text + ": " + str(temperature) + " ℃" + "\n"
-        answer += "Ветер: " + str(wind) + " м/с" + "\n"
-        answer += "В общем - " + w.detailed_status + "\n\n"
+        answer += "Ветер: " + str(wind) + " м/с " + "\n"
+        answer += "В общем - " + w.detailed_status + emojo + "\n\n"
 
         if temperature < 10:
             answer += "Сейчас очень холодно, одевайся!"
@@ -45,6 +58,7 @@ def send_echo(message):
     except Exception as e:
         error = str(e)
         bot.send_message(message.chat.id, error + ". Please try again!")
+
 
 
 bot.polling(none_stop=True)
